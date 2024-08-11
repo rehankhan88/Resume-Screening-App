c4f347519a2fe83c49933e289c1416127ab59525
rehankhan88 <rehan9732088@gmail.com>
2024-08-11 12:00:23 -0700

    loading models and add image

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
-<html>
+import streamlit as st
+import pickle 
+import re
+import nltk
 
-<table>
-    <title>good</title>
-</table>
-<body>
-    <h1>good</h1>
-</body>
-</html>
+# Download necessary NLTK data
+nltk.download('punkt')
+nltk.download('stopwords')
+
+# Loading models
+clf = pickle.load(open('clf.pkl', 'rb'))
+tfidf = pickle.load(open('tfidf.pkl', 'rb'))
+
+# Web app setup
+st.image('rrrr.jpg', width=100)  # Ensure 'rrrr.jpg' is in the same directory or provide the correct path
+
+def main():
+    st.title('Rehan Khan Resume Screening App')
+    st.file_uploader('Upload Resume', type=['txt', 'pdf'])
+
+
+#python main
+if __name__ == "__main__":
+    main()
